60307193d68bc0811f935546952bd2cc3ac27e5a
pepito <pepito_cochinito@gmail.com>
2022-06-08 11:58:48 -0400

    El css de option-1 ya esta listo, falta un detalle

--- src/Views/option-1.php
+++ src/Views/option-1.php
@@ -1,7 +1,26 @@
 <?php
-  //print_r($view->user);
-  echo " El nick ya esta en uso.";
-  ?>
-    <a href="<?=SITE_URL?>register">Volver al registro de usuario</a>
-  <?php
+//print_r($view->user);
+?>
+<html>
+  <head>
+    <link rel="stylesheet" href="http://prueba.ney/css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Carter+One&amp;display=swap" rel="stylesheet">
+
+    <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&amp;family=Itim&amp;family=Luckiest+Guy&amp;display=swap" rel="stylesheet">
+  </head>
+  <body>
+    <div class="box-light-blue">
+      <div class="box-one">
+        <div class="box-two">
+          <div class="box-tres">
+            <p class="text-p">El nick ya está en uso.</p>
+            <a class="text-a" onmouseover="colorText(this)" href="<?= SITE_URL ?>register">Volver al registro de usuario</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <script src="<?=SITE_URL?>js/script.js"></script>
+  </body>
+</html>
+<?php
 ?>
