2cbd9b53173fd0d9deb2cf64b7fd0b5594dfeb7c
pepito <pepito_cochinito@gmail.com>
2022-07-02 17:56:50 -0400

    Comentario en middlewares/user

--- src/Middlewares/User.php
+++ src/Middlewares/User.php
@@ -8,47 +8,51 @@ use Libs\Router;
 use Libs\Middleware;
 use Libs\View;
 
-// comprueba si el usuario es ese
 class User extends Middleware {
 
+    /**
+     * Revisa que el usuario este logueado.
+     *
+     * @param mixed $req
+     */
     public static function check($req) {
 
-    if (is_null($_COOKIE['notita']))
-      return Router::redirect('/login');
-    else
-       $token = $_COOKIE['notita'];
-    //Preguntar si el token esta en la BD
-    //where busca y getFirst retorna una coinsidencia (objeto)
-    $user = MUser::where('token', "$token")->getFirst();
+        if (is_null($_COOKIE['notita']))
+            return Router::redirect('/login');
+        else
+            $token = $_COOKIE['notita'];
+        //Preguntar si el token esta en la BD
+        //where busca y getFirst retorna una coinsidencia (objeto)
+        $user = MUser::where('token', "$token")->getFirst();
 
-    if(is_null($user))
-        return Router::redirect('/login');
+        if(is_null($user))
+            return Router::redirect('/login');
 
-    $req->user = $user;
-    $req->view = new View;
-    $req->view->avatar = md5(strtolower(trim($user->mail)));
+        $req->user = $user;
+        $req->view = new View;
+        $req->view->avatar = md5(strtolower(trim($user->mail)));
 
-    static::next($req);
+        static::next($req);
   }
 
-  //verificar dueño
-   public static function verifyOwner($req) {
+    //verificar dueño
+    public static function verifyOwner($req) {
 
-     $notita = MNotita::getById($req->params->id);
+        $notita = MNotita::getById($req->params->id);
 
-     if(is_null($notita))
-        exit('La nota no existe.');
-     ///////En estas lineas mandamos la nota a cargar en la pagina ////////
-     //$req->notita = $notita;
-     $req->view = new View;
-     $req->view->notita = $notita;
-     ////////////////////////////////////////////////////////////////////////
+        if(is_null($notita))
+            exit('La nota no existe.');
+        ///////En estas lineas mandamos la nota a cargar en la pagina ////////
+        //$req->notita = $notita;
+        $req->view = new View;
+        $req->view->notita = $notita;
+        ////////////////////////////////////////////////////////////////////////
 
-     if($notita->user_id == $req->user->id ||
-     $req->user->admin)
-       static::next($req);
-     else
-       exit('Acceso denegado.');
-   }
+        if($notita->user_id == $req->user->id ||
+           $req->user->admin)
+            static::next($req);
+        else
+            exit('Acceso denegado.');
+    }
 
 }
