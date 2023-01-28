cc61c9a9b89206df46232e768dcdcee5827a044d
Ney <nayla.gibson@gmail.com>
2023-01-28 07:58:02 -0400

    input text hover cyan

--- src/Views/login.php
+++ src/Views/login.php
@@ -12,9 +12,9 @@ include 'body.php';
           <p class="error-tip"><?=$view->error?></p>
         <?php endif; ?>
         <p class="titulo" for="name">Nombre:</p>
-        <input type="text" name="name" value="">
+        <input class="input-text" type="text" name="name" value="">
         <p class="titulo" for="password">Contraseña:</p>
-        <input type="password" name="password" value="">
+        <input class="input-text" type="password" name="password" value="">
         <input class="boton boton-login" onmouseover="SaveOver(this)" onmouseout="SaveOut(this)" type="submit" value="Save">
       </form>
     </div>
