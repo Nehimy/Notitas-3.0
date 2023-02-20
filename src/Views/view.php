7709f3bdd84d3f7fe6749ebfc58fdf6e10718b33
Ney <nayla.gibson@gmail.com>
2023-02-20 18:30:53 -0400

    view.php ya va tomando forma, falta centar el contenido de la nota

--- src/Views/view.php
+++ src/Views/view.php
@@ -1,30 +1,32 @@
 <?php
 include 'header.php';
 ?>
-qqqq
+
 <!-- Contenedor -->
-<!--<div id="sky-blue-set"> -->
-<!--  <div class="transparent-set"> -->
-<!--  <div class="white-set"> -->
-<!-- Ver nota indidual-->
-<div class="single-note <?=$view->notita->color?>">
-  <div class="content-button-emojis" >
-    <a class="eddit" href="<?=SITE_URL?>note/<?=$view->notita->id?>/edit">
-      <img onmouseover="bigImg(this)" onmouseout="normalImg(this)" src="<?=SITE_URL?>css/Edit.svg" alt="Editar notita" title="Editar notita">
-    </a>
-    <a class="delete" href="<?=SITE_URL?>note/<?=$view->notita->id?>/remove">
-      <img onmouseover="bigImg(this)" onmouseout="normalImg(this)" src="<?=SITE_URL?>css/Delete.svg" alt="Delete notita" title="Delete notita">
-    </a>
-  </div>
-  <div class="title-of-view title-box-view"> <?=$view->notita->title?> </div>
-  <div class="font">
-    <?=$view->notita->content?>
+<div class=" container-transparent">
+  <div class="white-transparent">
+    <div class="container-white">
+      <div class="white contenet-one-note">
+        <!-- Ver nota indidual-->
+        <div class="single-note <?=$view->notita->color?>">
+          <div class="content-button-emojis" >
+            <a class="eddit" href="<?=SITE_URL?>note/<?=$view->notita->id?>/edit">
+              <img onmouseover="bigImg(this)" onmouseout="normalImg(this)" src="<?=SITE_URL?>css/Edit.svg" alt="Editar notita" title="Editar notita">
+            </a>
+            <a class="delete" href="<?=SITE_URL?>note/<?=$view->notita->id?>/remove">
+              <img onmouseover="bigImg(this)" onmouseout="normalImg(this)" src="<?=SITE_URL?>css/Delete.svg" alt="Delete notita" title="Delete notita">
+            </a>
+          </div>
+          <div class="title-of-view title-box-view"> <?=$view->notita->title?> </div>
+          <div class="font">
+            <?=$view->notita->content?>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </div>
-  <!--  </div>  -->
-  <!-- </div> -->
-  <!--</div>-->
-  <script src="<?=SITE_URL?>js/script.js"></script>
+<script src="<?=SITE_URL?>js/script.js"></script>
 <?php
  include 'footer.html';
 ?>
