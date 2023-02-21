f3eaca3777df36f3e9fb7ae6cac7614ef700a188
Ney <nayla.gibson@gmail.com>
2023-02-21 10:09:34 -0400

    mejorando el alineamiento de una sola nota

--- src/Views/view.php
+++ src/Views/view.php
@@ -9,16 +9,21 @@ include 'header.php';
       <div class="white contenet-one-note">
         <!-- Ver nota indidual-->
         <div class="single-note <?=$view->notita->color?>">
-          <div class="content-button-emojis" >
-            <a class="eddit" href="<?=SITE_URL?>note/<?=$view->notita->id?>/edit">
-              <img onmouseover="bigImg(this)" onmouseout="normalImg(this)" src="<?=SITE_URL?>css/Edit.svg" alt="Editar notita" title="Editar notita">
-            </a>
-            <a class="delete" href="<?=SITE_URL?>note/<?=$view->notita->id?>/remove">
-              <img onmouseover="bigImg(this)" onmouseout="normalImg(this)" src="<?=SITE_URL?>css/Delete.svg" alt="Delete notita" title="Delete notita">
-            </a>
+          <div class="title-box top-container">
+            <div class="content-button-emojis" >
+              <a class="eddit" href="<?=SITE_URL?>note/<?=$view->notita->id?>/edit">
+                <img onmouseover="bigImg(this)" onmouseout="normalImg(this)" src="<?=SITE_URL?>css/Edit.svg" alt="Editar notita" title="Editar notita">
+              </a>
+              <a class="delete" href="<?=SITE_URL?>note/<?=$view->notita->id?>/remove">
+                <img onmouseover="bigImg(this)" onmouseout="normalImg(this)" src="<?=SITE_URL?>css/Delete.svg" alt="Delete notita" title="Delete notita">
+              </a>
+            </div>
+            <div class="title">
+              <?=$view->notita->title?>
+            </div>
+
           </div>
-          <div class="title-of-view title-box-view"> <?=$view->notita->title?> </div>
-          <div class="font">
+          <div class="text-content one-note">
             <?=$view->notita->content?>
           </div>
         </div>
