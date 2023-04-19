f9e48f32168b06571d9b7c0fa50bb0dec5f513cc
Ney <nayla.gibson@gmail.com>
2023-04-18 22:09:00 -0400

    editando el titulo en view.php

--- src/Views/view.php
+++ src/Views/view.php
@@ -1,21 +1,22 @@
-<div class="single-note <?=$view->notita->color?>">
-  <!-- Contenedor del título y los botón-emojis -->
-  <div class="top-container">
-    <div class="content-button-emojis" >
+<div class="container-see-note">
+  <div class="see-note single-note <?=$view->notita->color?>">
+    <div class="container-title">
       <a class="eddit" href="<?=SITE_URL?>note/<?=$view->notita->id?>/edit">
-        <img onmouseover="bigImg(this)" onmouseout="normalImg(this)" src="<?=SITE_URL?>css/Edit.svg" alt="Editar notita" title="Editar notita">
+        <img onmouseover="bigImg(this)" onmouseout="normalImg(this)"
+             src="<?=SITE_URL?>css/Edit.svg" alt="Editar notita" title="Editar notita">
       </a>
       <a class="delete" href="<?=SITE_URL?>note/<?=$view->notita->id?>/remove">
-        <img onmouseover="bigImg(this)" onmouseout="normalImg(this)" src="<?=SITE_URL?>css/Delete.svg" alt="Delete notita" title="Delete notita">
+        <img onmouseover="bigImg(this)" onmouseout="normalImg(this)"
+             src="<?=SITE_URL?>css/Delete.svg" alt="Delete notita" title="Delete notita">
       </a>
-    </div>
-    <div class="title">
-      <?=$view->notita->title?>
+      <div class="title">
+        <?=$view->notita->title?>
+      </div>
     </div>
 
-  </div>
-  <div class="text-content one-note">
-    <?=$view->notita->content?>
+    <div class="text-content one-note">
+      <?=$view->notita->content?>
+    </div>
   </div>
 </div>
 
