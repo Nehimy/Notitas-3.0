a9404f7734116d90e4f30434a16abb238ac55f2f
Ney <nayla.gibson@gmail.com>
2023-04-19 12:13:48 -0400

    falta alinear los emojis en view.php

--- src/Views/view.php
+++ src/Views/view.php
@@ -1,23 +1,32 @@
 <div class="container-see-note">
   <div class="see-note single-note <?=$view->notita->color?>">
+    <!-- container title -->
     <div class="container-title">
-      <a class="eddit" href="<?=SITE_URL?>note/<?=$view->notita->id?>/edit">
-        <img onmouseover="bigImg(this)" onmouseout="normalImg(this)"
-             src="<?=SITE_URL?>css/Edit.svg" alt="Editar notita" title="Editar notita">
-      </a>
-      <a class="delete" href="<?=SITE_URL?>note/<?=$view->notita->id?>/remove">
-        <img onmouseover="bigImg(this)" onmouseout="normalImg(this)"
-             src="<?=SITE_URL?>css/Delete.svg" alt="Delete notita" title="Delete notita">
-      </a>
-      <div class="title">
-        <?=$view->notita->title?>
+      <!-- container emoji -->
+      <div class="container-emoji">
+        <div class="container-eddit">
+          <a class="eddit" href="<?=SITE_URL?>note/<?=$view->notita->id?>/edit">
+            <img onmouseover="bigImg(this)" onmouseout="normalImg(this)"
+                 src="<?=SITE_URL?>css/Edit.svg" alt="Editar notita" title="Editar notita">
+          </a>
+        </div>
+        <div class="container-delete">
+          <a class="delete" href="<?=SITE_URL?>note/<?=$view->notita->id?>/remove">
+            <img onmouseover="bigImg(this)" onmouseout="normalImg(this)"
+                 src="<?=SITE_URL?>css/Delete.svg" alt="Delete notita" title="Delete notita">
+          </a>
+        </div>
+      </div>
+      <div class="second-container-title">
+        <div class="title">
+          <?=$view->notita->title?>
+        </div>
       </div>
     </div>
-
-    <div class="text-content one-note">
+    <div class="container-content-note">
       <?=$view->notita->content?>
     </div>
+
   </div>
-</div>
 
 <script src="<?=SITE_URL?>js/script.js"></script>
