cd43c6bab5a2c23567522270be145f886cb643b5
Ney <nayla.gibson@gmail.com>
2023-04-25 22:25:06 -0400

    areglando cosas

--- src/Views/panel-content.php
+++ src/Views/panel-content.php
@@ -2,41 +2,39 @@
   <div class="container-panel">
     <div class="panel">
       <!--Search-->
-      <p class="style-text-big border-top"></p>
-      <form  action="<?=SITE_URL?>panel-notes" method="GET">
-        <!-- <form  action="<?=SITE_URL?>panel-notes" method="GET"> -->
-        <div class="container-input-search-panel">
-          <input class="input-search-panel" placeholder="Search" type="text" name="search" required="required" value="" >
-          <input class="button-search-panel" type="submit" value="search">
-        </div>
-      </form>
-
-      <p class="style-text-big border-top"</p>
+      <p class="style-text-big border-top">
+        <form class="margin-botton"  action="<?=SITE_URL?>panel-notes" method="GET">
+          <div class="container-input-search-panel">
+            <input class="input-search-panel" placeholder="Search" type="text" name="search" required="required" value="" >
+            <input class="button-search-panel" type="submit" value="search">
+          </div>
+        </form>
+      </p>
       <!-- New note -->
-      <div class="padding-text">
+      <!-- <div class="padding-text"> -->
         <p class="border-top">
-          <a class="style-text-big  border-top" alt="Crea nueva notita" title="Crea nueva notita"
-                          href="<?=SITE_URL?>new"> New note </a>
+          <a class="style-text-big padding-text" alt="Crea nueva notita" title="Crea nueva notita"
+             href="<?=SITE_URL?>new"> New note </a>
         </p>
         <p class="border-top">
           <!-- Todas las notas -->
-          <a class="style-text-big" alt="Todos las notas" title="Todos las notas de Notitas"
+          <a class="style-text-big padding-text" alt="Todos las notas" title="Todos las notas de Notitas"
              href="<?=SITE_URL?>panel-notes">Todas las notas</a>
         </p>
         <p class="border-top">
           <!-- Todos los usuarios -->
-          <a class="style-text-big" alt="Todos los usuarios" title="Todos los users de Notitas"
+          <a class="style-text-big padding-text" alt="Todos los usuarios" title="Todos los users de Notitas"
              href="<?=SITE_URL?>panel-users">
             Todos los usuarios
           </a>
         </p>
         <p class="border-top">
-          <a class="style-text-big" alt="Todos los usuarios" title="Todos los users de Notitas"
+          <a class="style-text-big padding-text" alt="Todos los usuarios" title="Todos los users de Notitas"
              href="<?=SITE_URL?>all">
             Cuenta de usuario.
           </a>
         </p>
-      </div>
+        <!-- </div> -->
       <p class="style-text-big border-botton"</p>
 
 
